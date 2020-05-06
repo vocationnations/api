dbda78657eda27f56c58fa3120d092adef429c51
Tayab Soomro <isoomro.t@gmail.com>
2020-05-06 02:11:39 -0600

    update user interface - add user struct - add work struct - add education struct

--- src/user/interfaces.go
+++ src/user/interfaces.go
@@ -1,1 +1,26 @@
 package user
+
+// User defines the structure of user data
+type User struct {
+	Id	string `json:"id"`
+	Name string `json:"name"`
+	EducationHistory Education `json:"education"`
+	WorkHistory Work `json:"work"`
+}
+
+// Education defines the structure of education data
+type Education struct {
+	Id string `json:"id"`
+	DegreeTitle string `json:"degreetitle"`
+	Start string `json:"start"`
+	End string `json:"end"`
+	Institution string `json:"institution"`
+}
+
+type Work struct {
+	Id string `json:"id"`
+	Position string `json:"position"`
+	Organization string `json:"organization"`
+	Start string `json:"start"`
+	End string `json:"end"`
+}
