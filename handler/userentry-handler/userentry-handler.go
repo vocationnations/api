3c039c8d1de061e9193cd04e543d734e913890f8
MuatazBadr <m.alobaidy98@gmail.com>
2021-08-19 18:18:54 -0400

    update userentry-handler by filling GetUserEntry func

--- handler/userentry-handler/userentry-handler.go
+++ handler/userentry-handler/userentry-handler.go
@@ -3,9 +3,11 @@ package userentry_handler
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/vocationnations/api/helper"
 	"github.com/vocationnations/api/model"
 	"net/http"
+	"strconv"
 )
 
 func GetUserEntries(w http.ResponseWriter, r *http.Request, ctx helper.AppContext) error {
@@ -30,6 +32,24 @@ func GetUserEntries(w http.ResponseWriter, r *http.Request, ctx helper.AppContex
 }
 
 func GetUserEntry(w http.ResponseWriter, r *http.Request, ctx helper.AppContext) error {
-	// TODO: Implement me
+	params := mux.Vars(r)
+
+	var db = ctx.DB.GetDatabase()
+
+	// get the id from the url
+	idStr := params["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return fmt.Errorf("the id '%s' is not a valid integer", idStr)
+	}
+
+	uentry := &model.UserEntry{Id: id}
+
+	if err := db.Model(uentry).WherePK().Select(); err != nil {
+		return fmt.Errorf("user entry cannot be retrieved, err: %v", err)
+	}
+	if err := json.NewEncoder(w).Encode(uentry); err != nil {
+		return fmt.Errorf("cannot encode user entry, err: %v", err)
+	}
 	return nil
 }
